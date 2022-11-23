7ed2b7a8251bd011f59b88c43003dececdd5cedb
StevenIGuess <stefan.cubing242@gmail.com>
2022-11-23 15:05:11 +0100

    git commit suicide

--- Aufgaben/Blatt05.Kiehn.Van.Cezar/Aufgabe2/PlotCoords/plot_coords.py
+++ Aufgaben/Blatt05.Kiehn.Van.Cezar/Aufgabe2/PlotCoords/plot_coords.py
@@ -4,15 +4,20 @@ import sys
 plt.switch_backend('agg')
 x_list=[]
 y_list=[]
+
+if len(sys.argv) < 2:
+    sys.stderr.write("Usage: {} <filename.tsv>\n".format(sys.argv[0]))
+    exit(1)
+
 with open(sys.argv[1]) as stream:
     for line in stream.readlines():
         values=line.split('\t')
-        x_list.append(values[0])
-        y_list.append(values[1])
-fig, ax=plt.subplots(figsize=(3,2))
+        x_list.append(int(values[0]))
+        y_list.append(int(values[1]))
+fig, ax = plt.subplots(figsize=(3,2))
 ax.set_xticks([])
 ax.set_yticks([])
 for side in ['left','top','bottom','right']:
     ax.spines[side].set_visible(False)
-ax.scatter(x_list,y_list,s=10,color='#E8BEAC')
+ax.scatter(x_list,y_list,s=10,color='#ffd60a')
 fig.savefig('scatter_plot.pdf')
