be171a748183e958187c404cc7879ae5e12f2879
clk1006 <clk1006@web.de>
2022-12-14 14:59:39 +0100

    Aufgabe 2 fertig

--- Aufgaben/Blatt08.Kiehn.Van.Cezar/Aufgabe2/FormEmail/gen_email.py
+++ Aufgaben/Blatt08.Kiehn.Van.Cezar/Aufgabe2/FormEmail/gen_email.py
@@ -9,6 +9,10 @@ def read_variables(filename):
     sys.stderr.write('{}: {}\n'.format(sys.argv[0],err))
     exit(1)
   # add your code here
+  varmap=dict()
+  for line in stream.readlines():
+    res=re.search(r"^(_[A-Z]+_)\t(.+)\n$",line)
+    varmap[res.group(1)]=res.group(2)
   stream.close()
   # return value represents variable names and their values
   return varmap   
@@ -20,6 +24,12 @@ def substitute_variables(varmap,filename):
     sys.stderr.write('{}: {}\n'.format(sys.argv[0],err))
     exit(1)
   # add your code here
+  for line in stream.readlines():
+    for match in re.findall(r"\w+|\W+",line):
+      if re.match(r"_[A-Z]+_",match):
+        print(varmap[match],end="")
+      else:
+        print(match,end="")
   stream.close()
 
 if len(sys.argv) != 3:
@@ -28,6 +38,8 @@ if len(sys.argv) != 3:
   exit(1)
 
 varmapfile = sys.argv[1]
+#varmapfile = "Aufgabe2/FormEmail/patient1.tsv"
 varmap = read_variables(varmapfile)
 textfilename = sys.argv[2]
+#textfilename = "Aufgabe2/FormEmail/email_generisch.txt"
 substitute_variables(varmap,textfilename)
