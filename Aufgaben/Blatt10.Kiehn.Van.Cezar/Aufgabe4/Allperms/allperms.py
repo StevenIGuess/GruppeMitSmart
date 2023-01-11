dc85fd47b4e07cfb9fed8b57d60883b229fc2107
clk1006 <clk1006@web.de>
2023-01-11 15:35:06 +0100

    Programm für B10A4 fertig

--- Aufgaben/Blatt10.Kiehn.Van.Cezar/Aufgabe4/Allperms/allperms.py
+++ Aufgaben/Blatt10.Kiehn.Van.Cezar/Aufgabe4/Allperms/allperms.py
@@ -3,13 +3,25 @@
 from math import factorial
 import sys, argparse
 
+def all_perms_rec(elems):
+  if len(elems)<2:
+    return [elems.copy()]
+  perms=[]
+  for i,el in enumerate(elems):
+    elems.pop(i)
+    perms_new=all_perms_rec(elems)
+    for perm in perms_new:
+      perm.append(el)
+      perms.append(perm)
+    elems.insert(i,el)
+  return perms  
 def all_permutations(elems):
-  # to be implemented
-  return
+  return all_perms_rec(elems)
 
 def all_permutations_verify(all_perms,elems):
-  # to be implemented
-  return
+  assert len(all_perms)==factorial(len(elems))
+  assert len(set(map(lambda x:tuple(x),all_perms)))==len(all_perms)
+  assert all([set(perm)==set(elems) for perm in all_perms])
 
 def parse_command_line(argv):
   p = argparse.ArgumentParser(description='generate and verify permutations')
